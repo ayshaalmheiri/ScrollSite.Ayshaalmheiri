5134f823877251b7a370d93ba9589b10765fbee9
ayshaalmheiri <144939765+ayshaalmheiri@users.noreply.github.com>
2023-10-08 21:07:28 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function scrollPage(){
+	if(this.id === 'first-page'){
+		window.scrollTo({
+  			top: window.innerHeight * 2,
+  			left: 0,
+  			behavior: 'smooth'
+		});
+		}
+	}
+
+function scrollPage(){
+	if(this.id === 'second-page'){
+		window.scrollTo({
+  			top: window.innerHeight * 2,
+  			left: 0,
+  			behavior: 'smooth'
+		});
+		}
+	}
+
+function scrollPage(){
+	if(this.id === 'third-page'){
+		window.scrollTo({
+  			top: window.innerHeight * 2,
+  			left: 0,
+  			behavior: 'smooth'
+		});
+	}
+
+	if(this.id === 'fourth-page'){
+		window.scrollTo({
+  			top: window.innerHeight * 3,
+  			left: 0,
+  			behavior: 'smooth'
+		});
+	}
+}
+
+
